df289e58ecdd9eb6723d040ff47ba8ee27ff458e
Misaka10089 <72312207+Misaka10089@users.noreply.github.com>
2022-03-10 10:23:38 +0800

    test

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2021 The PixelExperience Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/exthm_pdx206.mk
+
+COMMON_LUNCH_CHOICES := \
+    exthm_pdx206-user \
+    exthm_pdx206-userdebug \
+    exthm_pdx206-eng
---
+++ exthm_pdx206.mk
@@ -0,0 +1,46 @@
+#
+# Copyright (C) 2021 The Arrow-OS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from common.mk
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+# Inherit some common exTHmui stuff
+$(call inherit-product, vendor/exthm/config/common.mk)
+
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+WITH_GAPPS := true
+IS_PHONE := true
+
+# Gapps
+EXTHM_GAPPS := true
+
+PRODUCT_BRAND := Sony
+PRODUCT_DEVICE := pdx206
+PRODUCT_MANUFACTURER := Sony
+PRODUCT_NAME := exthm_pdx206
+PRODUCT_MODEL := Xperia 5 II
+PRODUCT_GMS_CLIENTID_BASE := android-sonymobile
+
+TARGET_VENDOR_PRODUCT_NAME := pdx206
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="Sony/pdx206/pdx2062 10 QKQ1.200108.002 1:user release-keys"
+BUILD_FINGERPRINT := Sony/pdx206/pdx206:10/QKQ1.200108.002/1:user/release-keys
